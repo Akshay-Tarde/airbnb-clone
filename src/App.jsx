cd1cabe9d9d06d29662d57c060c8fccc64cdd441
Akshay-Tarde <akshaymtarde@gmail.com>
2023-03-01 23:54:07 +0530

    Used props to pass the data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-
+import katieImgFromApp from "./assets/katie-zaferes.png"
 export default function App() {
   return (
     <div className="main-container">
@@ -12,7 +12,8 @@ export default function App() {
       <main>
         <Hero/>
           <section className="card-section container">
-            <Card />
+            <Card img={katieImgFromApp} rating={5.0} reviewCount={6} country="Australia" 
+                  title="Life lessons with Katie Zaferes" price={136} />
           </section>
       </main>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,19 @@ import katieImg from '../assets/katie-zaferes.png';
 import star from "../assets/star-1.png";
 
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props);
     return (
         <div className="card">
             <img src={katieImg} className="card-image" />
             <div className="card-stats">
                 <img src={star} className="card-star" />
-                <span>5.0   </span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
