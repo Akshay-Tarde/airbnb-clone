d752c4b0aeafbc3974028bd989742751fb256dc8
Akshay-Tarde <akshaymtarde@gmail.com>
2023-03-03 00:20:28 +0530

    Styled the array of card components.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ export default function App() {
   const cardComponents = data.map( item => <Card key={item.id} img={item.coverImg} rating={item.stats.rating} 
                                             reviewCount={item.stats.reviewCount} location={item.location}
                                             title={item.title} description={item.description}
+                                            price={item.price}
                                             />)
   return (
     <div className="main-container">
