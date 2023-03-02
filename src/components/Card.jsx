cc0b4c4e0feafa6b2041b1f45ae6182ba3ac91e0
Akshay-Tarde <akshaymtarde@gmail.com>
2023-03-03 00:53:30 +0530

    Added the logic to display either online or sold out in the badge.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,22 @@ import React from "react"
 
 
 export default function Card(props) {
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    }
+    else if(props.location === 'Online') {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
             <img src={`./assets/${props.img}`} className="card-image" />
-            {props.openSpots || <p className="sold-out">SOLD OUT</p>}
+            { badgeText && <p className="sold-out">{badgeText}</p>}
             <div className="card-stats">
                 <img src="./assets/star-1.png" className="card-star" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
